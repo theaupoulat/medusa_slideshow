ab8449720c8b4159f6050b9b147618e1b43f2c02
Theau Poulat <theau.poulat@gmail.com>
2025-11-10 17:21:57 +0100

    feat: customize slide color

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import { useState } from "react";
 import "./App.css";
+import { slideColors } from "./colors";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./components/ui/select";
 import { Slider } from "./components/ui/slider";
+
+const buildSelectOptions = () => {
+  return slideColors.map(({ hex, name }) => (
+    <SelectItem value={hex}>
+      <div
+        className="rounded-md w-[12px] h-[12px]"
+        style={{ backgroundColor: hex }}
+      ></div>{" "}
+      {name}
+    </SelectItem>
+  ));
+};
 
 function App() {
   const [textPosition, setTextPosition] = useState(0);
   const [contentAngle, setContentAngle] = useState(0);
   const [contentPosition, setContentPosition] = useState(0);
+  const [slideColor, setSlideColor] = useState(slideColors[0].hex);
+
+  const selectOptions = buildSelectOptions();
+
   return (
     <div className="p-9 flex flex-col items-center gap-2.5">
       <div id="slider-content" className="w-3xs">
@@ -32,12 +56,22 @@ function App() {
           defaultValue={[0]}
           onValueChange={(e) => setContentPosition(e[0])}
         />
+        <span> Pick color </span>
+        <Select onValueChange={(e) => setSlideColor(e)}>
+          <SelectTrigger className="w-[280px]" value={slideColor}>
+            <SelectValue placeholder="Select a color" />
+          </SelectTrigger>
+          <SelectContent>{...selectOptions}</SelectContent>
+        </Select>
       </div>
       <div
         id="slide"
         className="overflow-clip m-auto w-[800px] h-[1000px] bg-sky-500 flex flex-col items-center justify-between pt-5 "
+        style={{
+          backgroundColor: slideColor,
+        }}
       >
-        <div id="title-content" className="flex flex-col items-center gap-2">
+        <div id="title" className="flex flex-col items-center gap-2">
           <div id="text" className="uppercase text-4xl font-semibold mt-11">
             zyed et bouna: 20 ans
           </div>
@@ -61,7 +95,7 @@ function App() {
           </div>
           <div
             id="screenshot"
-            className="bg-green-400 w-[900px] h-[450px] rounded-2xl"
+            className="bg-purple-500 w-[900px] h-[450px] rounded-2xl"
           ></div>
         </div>
       </div>
---
+++ src/colors.ts
@@ -0,0 +1,22 @@
+export const slideColors = [
+  {
+    hex: "#95CF84",
+    name: "eelvert",
+  },
+  {
+    hex: "#9BACD2",
+    name: "bleu démocrates chrétiens",
+  },
+  {
+    hex: "#FFEFB9",
+    name: "sable",
+  },
+  {
+    hex: "#2B6C40",
+    name: "vert palestine",
+  },
+  {
+    hex: "#A93C40",
+    name: "rouge brun",
+  },
+];
