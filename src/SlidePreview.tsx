9f31f7b3ea1cdbc3a23d08c9549ea843154d12eb
Theau Poulat <theau.poulat@gmail.com>
2025-11-10 18:41:34 +0100

    refactor: move slide preview to own file

---
+++ src/SlidePreview.tsx
@@ -0,0 +1,65 @@
+import { isColorDark } from "./colors";
+import { useSettings } from "./SlideSettingsProvider";
+
+export const SlidePreview = () => {
+  const settings = useSettings();
+
+  if (!settings) {
+    return;
+  }
+
+  const { slideColor, textPosition, contentAngle, contentPosition } = settings;
+
+  const textColor = isColorDark(slideColor) ? "#FFFFFF" : "#000000";
+  return (
+    <div
+      id="slide"
+      className="overflow-clip m-auto w-[800px] h-[1000px] bg-sky-500 flex flex-col items-center justify-between pt-5 "
+      style={{
+        backgroundColor: slideColor,
+      }}
+    >
+      <div id="header" className="flex flex-col items-center gap-2">
+        <div
+          id="title"
+          style={{
+            color: textColor,
+          }}
+          className="uppercase text-4xl font-semibold mt-11"
+        >
+          zyed et bouna: 20 ans
+        </div>
+        <div
+          id="line"
+          style={{
+            borderColor: textColor,
+          }}
+          className="border-b-2 w-[400px]"
+        ></div>
+      </div>
+
+      <div
+        id="content"
+        className=" flex flex-col items-center"
+        style={{
+          transform: `rotate(${-90 - contentAngle}deg) translateY(${contentPosition}px)`,
+        }}
+      >
+        <div
+          id="media_name"
+          className=" uppercase text-4xl"
+          style={{
+            transform: `translateX(${textPosition}px)`,
+            color: textColor,
+          }}
+        >
+          a la tv sur ma tv
+        </div>
+        <div
+          id="screenshot"
+          className="bg-purple-500 w-[900px] h-[450px] rounded-2xl"
+        ></div>
+      </div>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-import { SlideSettingsProvider } from "./SlideSettingsProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <SlideSettingsProvider>
-      <App />
-    </SlideSettingsProvider>
+    <App />
   </StrictMode>,
 );
