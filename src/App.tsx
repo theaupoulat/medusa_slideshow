97f5c4869fa4444cf9075bb4bdb97d8c030469b9
Theau Poulat <theau.poulat@gmail.com>
2025-11-11 01:16:27 +0100

    feat: add dark/light toggle mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import "./App.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 import { SlidePreview } from "./SlidePreview/SlidePreview";
 import { Settings } from "./SlideSettings/SlideSettings";
 import { SlideSettingsProvider } from "./SlideSettings/SlideSettingsProvider";
 
 function App() {
   return (
-    <SlideSettingsProvider>
-      <div className="p-9 flex flex items-center gap-2.5 bg-slate-300">
-        <Settings />
-        <SlidePreview />
-      </div>
-    </SlideSettingsProvider>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <ModeToggle />
+      <SlideSettingsProvider>
+        <div className="p-9 flex items-center gap-2.5 bg-slate-300">
+          <Settings />
+          <SlidePreview />
+        </div>
+      </SlideSettingsProvider>
+    </ThemeProvider>
   );
 }
 
